20daab8543cbce6fa946a8733881e8e2a92e293b
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-01-07 01:47:57 +0330

    Folder Structure

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <span>This is app</span>
+    </div>
+  );
+};
+
+export default App;
