01231d6ff6178eeacca41107a222a1121cb4f383
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-01-07 11:44:48 +0330

    provide appolo and graphql

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
 import App from "./App";
 
+const client = new ApolloClient({
+  uri: "https://api-us-west-2.hygraph.com/v2/clcl380tk21rj01ugb5hv7809/master",
+  cache: new InMemoryCache(),
+});
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
