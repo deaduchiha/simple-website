15aa1f9f107f26f0be0ae8c666314af9cee3a412
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-01-08 13:23:33 +0330

    create cardEL components and useQuery for get data from graphQL

--- src/components/home/blog/Blog.jsx
+++ src/components/home/blog/Blog.jsx
@@ -1,10 +1,24 @@
 import React from "react";
+import { useQuery } from "@apollo/client";
+
+import { GET_BLOGS_INFO } from "../../../graphql/queries";
+import { Grid } from "@mui/material";
+import CardEL from "../../shared/CardEL";
 
 const Blog = () => {
+  const { loading, data, errors } = useQuery(GET_BLOGS_INFO);
+
+  if (loading) return <h5>Loading...</h5>;
+  if (errors) return <h5>Error</h5>;
+
   return (
-    <div>
-      <span>this is blog</span>
-    </div>
+    <Grid container spacing={2}>
+      {data.posts.map((post) => (
+        <Grid key={post.id} item xs={12} sm={6} md={4}>
+          <CardEL {...post} />
+        </Grid>
+      ))}
+    </Grid>
   );
 };
 
---
+++ src/components/shared/CardEL.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CardEL = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <span>this is card</span>
+    </div>
+  );
+};
+
+export default CardEL;
