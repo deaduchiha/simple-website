a68dceec327fe03c98b8d424dd992a573e715ac5
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-01-08 14:24:51 +0330

    create cardEL and add author queries

--- src/components/home/author/Authors.jsx
+++ src/components/home/author/Authors.jsx
@@ -1,6 +1,13 @@
 import React from "react";
+import { useQuery } from "@apollo/client";
+
+import { GET_AUTHOR } from "../../../graphql/queries";
 
 const Authors = () => {
+  const { loading, data, errors } = useQuery(GET_AUTHOR);
+  if (loading) return <h5>Loading...</h5>;
+  if (errors) return <h5>Error</h5>;
+  console.log(data);
   return (
     <div>
       <span>this is author</span>
--- src/components/shared/CardEL.jsx
+++ src/components/shared/CardEL.jsx
@@ -1,11 +1,60 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import {
+  Avatar,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  Divider,
+  Typography,
+} from "@mui/material";
 
-const CardEL = (props) => {
-  console.log(props);
+const CardEL = ({ author, slug, coverPhoto, title }) => {
   return (
-    <div>
-      <span>this is card</span>
-    </div>
+    <Card sx={{ borderRadius: 4 }}>
+      <CardHeader
+        avatar={<Avatar src={author.avatar.url} />}
+        title={
+          <Typography component="p" variant="p" color={"text.secondary"}>
+            {author.name}
+          </Typography>
+        }
+      />
+      <CardMedia
+        component="img"
+        height="194"
+        image={coverPhoto.url}
+        alt={slug}
+      />
+      <CardContent>
+        <Typography
+          component="h3"
+          variant="h6"
+          color="text.primary"
+          fontWeight={600}
+        >
+          {title}
+        </Typography>
+      </CardContent>
+      <Divider variant="middle" sx={{ margin: "10px" }} />
+      <CardActions>
+        {/* <Link
+          to={`/blogs/${slug}`}
+          style={{ textDecoration: "none", width: "100%" }}
+        > */}
+        <Button
+          variant="outlined"
+          size="small"
+          sx={{ width: "100%", borderRadius: 3 }}
+        >
+          Read more
+        </Button>
+        {/* </Link> */}
+      </CardActions>
+    </Card>
   );
 };
 
