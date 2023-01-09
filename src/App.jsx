10813aaddc13776914e4c5f5df486ef141288b07
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-01-09 12:28:02 +0330

    fix scroll to top

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,13 @@ import HomePage from "./components/home/HomePage";
 import Layout from "./components/layout/Layout";
 import BlogPage from "./components/home/blog/BlogPage";
 import AuthorPage from "./components/home/author/AuthorPage";
+import ScrollTop from "./components/shared/ScrollTop";
 
 const App = () => {
   return (
     <>
       <Layout>
+        <ScrollTop />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/blogs/:slug" element={<BlogPage />} />
